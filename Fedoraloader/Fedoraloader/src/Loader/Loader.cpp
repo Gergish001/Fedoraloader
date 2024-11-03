6a2c3c981b498355d29d20f953c899d71fb43d11
Gergish <g.berdnikov674@gmail.com>
2024-11-03 21:29:44 +0700

    Update Loader.cpp

--- Fedoraloader/Fedoraloader/src/Loader/Loader.cpp
+++ Fedoraloader/Fedoraloader/src/Loader/Loader.cpp
@@ -72,7 +72,7 @@ bool Loader::Load(const LaunchInfo& launchInfo)
 
 	// Find the game
 	Log::Info("Waiting for game...");
-	const HANDLE hGame = Utils::WaitForProcessHandle("tf_win64.exe", 90);
+	const HANDLE hGame = Utils::WaitForProcessHandle("tf.exe", 90);
 	if (hGame == INVALID_HANDLE_VALUE || hGame == nullptr)
 	{
 		throw std::runtime_error("Timeout while waiting for game");
@@ -91,7 +91,7 @@ bool Loader::Debug(const LaunchInfo& launchInfo)
 
 	// Find the game
 	Log::Info("Waiting for game...");
-	const HANDLE hGame = Utils::GetProcessHandle("tf_win64.exe");
+	const HANDLE hGame = Utils::GetProcessHandle("tf.exe");
 	if (hGame == INVALID_HANDLE_VALUE || hGame == nullptr)
 	{
 		throw std::runtime_error("Failed to get game handle");
